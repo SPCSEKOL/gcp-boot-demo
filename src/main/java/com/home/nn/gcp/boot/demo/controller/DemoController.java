1970ea34a0e0ea5af789d889890fc6979f99c8fb
Sudeepta Pal <pal.sudeepta@gmail.com>
2022-05-28 11:21:23 +0530

    gcp-boot-demo: Upto 2 decimal point precision
    Changed the result value upto 2 decimal point in DemoController

--- src/main/java/com/home/nn/gcp/boot/demo/controller/DemoController.java
+++ src/main/java/com/home/nn/gcp/boot/demo/controller/DemoController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DecimalFormat;
+
 @RestController
 public class DemoController {
 
     @GetMapping("getCentigradeTemp/{fahrenheit}")
-    public String getCentigradeTemperature(@PathVariable("fahrenheit")float far) {
-        float centigrade = (5*(far-32))/9;
-        return String.valueOf(centigrade);
+    public String getCentigradeTemperature(@PathVariable("fahrenheit")double far) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        double centigrade = (5*(far-32))/9;
+        return df.format(centigrade);
     }
 }
